3ea3a350a7646f34e8b3caf98bb2bd68790fe2dc
hpazy <hpazy@softserveinc.com>
2023-07-26 14:26:02 +0300

    C#2 - UC#23 Infrastructure setup

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>titles-credits-test-data-generator</h1>
+    </div>
+  );
+}
+
+export default App;
